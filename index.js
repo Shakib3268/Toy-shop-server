cc0f7dfdde78a26da434ff18015e0b43cff72c04
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-05-22 22:42:18 +0600

    search section

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
      client.connect();
     const toyCollection = client.db("toysPortal").collection("toys")
+    
+    const indexKeys = {category : 1, Name : 1}
+    const indexOptions = {name: "nameSub"}
+
+    const result = await toyCollection.createIndex(indexKeys,indexOptions)
+
+
+    app.get('/toySearch/:text',async (req,res) => {
+      const searchText = req.params.text;
+      const result = await toyCollection.find({
+        $or:[
+          {name:{$regex:searchText, $options:"i"}},
+          {category: {$regex: searchText, $options:"i"}},
+        ]
+      }).toArray()
+      res.send(result)
+    })
 
     app.get('/singletoy/:id',async(req,res) =>{
       const id = req.params.id
@@ -48,12 +65,11 @@ async function run() {
       res.send(toy)
     })
 
-
     app.get('/alltoy',async(req,res) =>{
       const result = await toyCollection.find({}).sort({price: -1}).toArray()
       res.send(result)
     })
-
+    //add toy by value
     app.post("/addtoy",async (req,res) => {
         const body = req.body
         if(!body){
@@ -62,7 +78,7 @@ async function run() {
         const result = await toyCollection.insertOne(body)
         res.send(result)
     })
-
+    // deleted toys
     app.delete('/mytoy/:id',async (req,res) =>{
       const id = req.params.id 
       const query = {_id : new ObjectId(id)}
